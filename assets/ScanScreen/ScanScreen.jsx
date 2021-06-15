ce8867f1ad2dd7e8501086c3d3ec43e03ba88d09
alekszych <aleks.zych@icloud.com>
2021-06-15 18:56:36 +0200

    czytnik kodów qr

--- assets/ScanScreen/ScanScreen.jsx
+++ assets/ScanScreen/ScanScreen.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import GestureRecognizer from "react-native-swipe-gestures";
 
 const styles = StyleSheet.create({
-    main: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+    camera: {
+        height: "50%",
+        width: "100%",
+    }
 });
 
 function ScanScreen({ navigation }) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
     return (
         <GestureRecognizer
             onSwipeLeft={() => navigation.goBack()}
             config={{velocityThreshold: 0.3, directionalOffsetThreshold: 80}}
             style={{flex: 1}}
         >
-            <View style={styles.main}>
-                <Text style={{fontSize: 20}}> Scan </Text>
-            </View>
+            <BarCodeScanner
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                style={styles.camera}
+            />
+            {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
         </GestureRecognizer>
     );
 }
