a89d9668e25ea8ca102d20aac7bd6246a97f9a91
alekszych <aleks.zych@icloud.com>
2021-06-17 18:08:42 +0200

    naprawione gesty samsung v2

--- App.js
+++ App.js
@@ -5,12 +5,13 @@ import HomeScreen from "./assets/HomeScreen/HomeScreen";
 import LoginScreen from "./assets/AuthScreen/LoginScreen";
 import ScanScreen from "./assets/ScanScreen/ScanScreen";
 import UserScreen from "./assets/AuthScreen/UserScreen";
+import {vh, vw} from "react-native-expo-viewport-units";
 const Stack = createStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer style={{flex: 1}}>
-            <Stack.Navigator initialRouteName="Home" style={{flex: 1}}>
+        <NavigationContainer style={{width: vw(100), height: vh(100)}}>
+            <Stack.Navigator initialRouteName="Home" style={{width: vw(100), height: vh(100)}}>
                 <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
                 <Stack.Screen name="Scan" component={ScanScreen} options={{ gestureDirection: "horizontal-inverted", headerShown: false }} />
                 <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
